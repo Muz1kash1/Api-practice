00166e47534424596c6e40bfd2359609f4805710
Denis_Shevchenko <muz1kash1@yandex.ru>
2022-08-09 17:18:29 +0300

    feat: init comit
    #INTERNSHIP-51

---
+++ src/main/java/com/example/apiweek5/Apiweek5Application.java
@@ -0,0 +1,13 @@
+package com.example.apiweek5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiweek5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apiweek5Application.class, args);
+    }
+
+}
